ced984d120d706184e7bb0fd00aee08dd00bc649
Akmitliviy <akmitliviy@gmail.com>
2023-11-18 01:36:19 +0300

    Session Class added

---
+++ Source/Kursova/ServerLogic/SessionSubsystem.cpp
@@ -0,0 +1,329 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SessionSubsystem.h"
+#include "OnlineSubsystemUtils.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+USessionSubsystem::USessionSubsystem()
+	: CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionCompleted))
+	, UpdateSessionCompleteDelegate(FOnUpdateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnUpdateSessionCompleted))
+	, StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnStartSessionCompleted))
+	, EndSessionCompleteDelegate(FOnEndSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnEndSessionCompleted))
+	, DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &ThisClass::OnDestroySessionCompleted))
+	, FindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this, &ThisClass::OnFindSessionsCompleted))
+	, JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnJoinSessionCompleted))
+{
+}
+
+bool USessionSubsystem::SessionHasBeenStarted()
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	//EOnlineSessionState::Type SessionState = SessionInterface->GetSessionState(NAME_GameSession);
+	bool bIsValidSession = SessionInterface.IsValid();
+
+	if (bIsValidSession)
+	{
+		EOnlineSessionState::Type SessionState = SessionInterface->GetSessionState(NAME_GameSession);
+		return SessionState >= EOnlineSessionState::Pending;
+	}
+	else
+	{
+		return false; // або що потрібно повернути, якщо сеанс недійсний
+	}
+	
+	//return SessionState >= EOnlineSessionState::Pending();
+}
+
+void USessionSubsystem::CreateSession(int32 NumPublicConnections, bool IsLANMatch)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnCreateSessionCompleteEvent.Broadcast(false);
+		return;
+	}
+
+	LastSessionSettings = MakeShared<FOnlineSessionSettings>();
+	LastSessionSettings->NumPrivateConnections = 0;
+	LastSessionSettings->NumPublicConnections = NumPublicConnections;
+	LastSessionSettings->bAllowInvites = true;
+	LastSessionSettings->bAllowJoinInProgress = true;
+	LastSessionSettings->bAllowJoinViaPresence = true;
+	LastSessionSettings->bAllowJoinViaPresenceFriendsOnly = true;
+	LastSessionSettings->bIsDedicated = false;
+	LastSessionSettings->bUsesPresence = true;
+	LastSessionSettings->bIsLANMatch = IsLANMatch;
+	LastSessionSettings->bShouldAdvertise = true;
+
+	LastSessionSettings->Set(SETTING_MAPNAME, FString("TestLevel"), EOnlineDataAdvertisementType::ViaOnlineService);
+
+	CreateSessionCompleteDelegateHandle = SessionInterface->AddOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegate);
+
+	const ULocalPlayer* localPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+	if (!SessionInterface->CreateSession(*localPlayer->GetPreferredUniqueNetId(), NAME_GameSession, *LastSessionSettings))
+	{
+		SessionInterface->ClearOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegateHandle);
+
+		OnCreateSessionCompleteEvent.Broadcast(false);
+	}
+}
+
+void USessionSubsystem::OnCreateSessionCompleted(FName SessionName, bool Successful)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (SessionInterface)
+	{
+		SessionInterface->ClearOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegateHandle);
+	}
+
+	OnCreateSessionCompleteEvent.Broadcast(Successful);
+}
+
+void USessionSubsystem::UpdateSession()
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnUpdateSessionCompleteEvent.Broadcast(false);
+		return;
+	}
+
+	TSharedPtr<FOnlineSessionSettings> UpdatedSessionSettings = MakeShared<FOnlineSessionSettings>(*LastSessionSettings);
+	UpdatedSessionSettings->Set(SETTING_MAPNAME, FString("Updated Level Name"), EOnlineDataAdvertisementType::ViaOnlineService);
+
+	UpdateSessionCompleteDelegateHandle =
+		SessionInterface->AddOnUpdateSessionCompleteDelegate_Handle(UpdateSessionCompleteDelegate);
+
+	if (!SessionInterface->UpdateSession(NAME_GameSession, *UpdatedSessionSettings))
+	{
+		SessionInterface->ClearOnUpdateSessionCompleteDelegate_Handle(UpdateSessionCompleteDelegateHandle);
+
+		OnUpdateSessionCompleteEvent.Broadcast(false);
+	}
+	else
+	{
+		LastSessionSettings = UpdatedSessionSettings;
+	}
+}
+
+void USessionSubsystem::OnUpdateSessionCompleted(FName SessionName, bool Successful)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (SessionInterface)
+	{
+		SessionInterface->ClearOnUpdateSessionCompleteDelegate_Handle(UpdateSessionCompleteDelegateHandle);
+	}
+
+	OnUpdateSessionCompleteEvent.Broadcast(Successful);
+}
+
+void USessionSubsystem::StartSession()
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnStartSessionCompleteEvent.Broadcast(false);
+		return;
+	}
+
+	StartSessionCompleteDelegateHandle =
+		SessionInterface->AddOnStartSessionCompleteDelegate_Handle(StartSessionCompleteDelegate);
+
+	if (!SessionInterface->StartSession(NAME_GameSession))
+	{
+		SessionInterface->ClearOnStartSessionCompleteDelegate_Handle(StartSessionCompleteDelegateHandle);
+
+		OnStartSessionCompleteEvent.Broadcast(false);
+	}
+}
+
+void USessionSubsystem::OnStartSessionCompleted(FName SessionName, bool Successful)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (SessionInterface)
+	{
+		SessionInterface->ClearOnStartSessionCompleteDelegate_Handle(StartSessionCompleteDelegateHandle);
+	}
+
+	if (Successful)
+	{
+		FString ConnectString("TestLevel?listen");
+		if (!GetWorld()->ServerTravel(ConnectString, true))
+		{
+			UKismetSystemLibrary::PrintString(this, FString("Error: server travel"));
+		}
+	}
+
+	OnStartSessionCompleteEvent.Broadcast(Successful);
+}
+
+void USessionSubsystem::EndSession()
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnEndSessionCompleteEvent.Broadcast(false);
+		return;
+	}
+
+	EndSessionCompleteDelegateHandle =
+		SessionInterface->AddOnEndSessionCompleteDelegate_Handle(EndSessionCompleteDelegate);
+
+	if (!SessionInterface->EndSession(NAME_GameSession))
+	{
+		SessionInterface->ClearOnEndSessionCompleteDelegate_Handle(EndSessionCompleteDelegateHandle);
+
+		OnEndSessionCompleteEvent.Broadcast(false);
+	}
+}
+
+void USessionSubsystem::OnEndSessionCompleted(FName SessionName, bool Successful)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (SessionInterface)
+	{
+		SessionInterface->ClearOnEndSessionCompleteDelegate_Handle(EndSessionCompleteDelegateHandle);
+	}
+
+	OnEndSessionCompleteEvent.Broadcast(Successful);
+}
+
+void USessionSubsystem::DestroySession()
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnDestroySessionCompleteEvent.Broadcast(false);
+		return;
+	}
+
+	DestroySessionCompleteDelegateHandle =
+		SessionInterface->AddOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegate);
+
+	if (!SessionInterface->DestroySession(NAME_GameSession))
+	{
+		SessionInterface->ClearOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegateHandle);
+
+		OnDestroySessionCompleteEvent.Broadcast(false);
+	}
+}
+
+void USessionSubsystem::OnDestroySessionCompleted(FName SessionName, bool Successful)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (SessionInterface)
+	{
+		SessionInterface->ClearOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegateHandle);
+	}
+
+	OnDestroySessionCompleteEvent.Broadcast(Successful);
+}
+
+void USessionSubsystem::FindSessions(int32 MaxSearchResults, bool IsLANQuery)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnFindSessionsCompleteEvent.Broadcast(TArray<FBlueprintSessionResult>(), false);
+		return;
+	}
+
+	FindSessionsCompleteDelegateHandle =
+		SessionInterface->AddOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegate);
+
+	LastSessionSearch = MakeShared<FOnlineSessionSearch>();
+	LastSessionSearch->MaxSearchResults = MaxSearchResults;
+	LastSessionSearch->bIsLanQuery = IsLANQuery;
+
+	LastSessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
+
+	const ULocalPlayer* localPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+	if (!SessionInterface->FindSessions(*localPlayer->GetPreferredUniqueNetId(), LastSessionSearch.ToSharedRef()))
+	{
+		SessionInterface->ClearOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegateHandle);
+
+		OnFindSessionsCompleteEvent.Broadcast(TArray<FBlueprintSessionResult>(), false);
+	}
+}
+
+void USessionSubsystem::OnFindSessionsCompleted(bool Successful)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (SessionInterface)
+	{
+		SessionInterface->ClearOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegateHandle);
+	}
+
+	if (LastSessionSearch->SearchResults.Num() <= 0)
+	{
+		OnFindSessionsCompleteEvent.Broadcast(TArray<FBlueprintSessionResult>(), Successful);
+		return;
+	}
+
+	TArray<FBlueprintSessionResult> BlueprintSessionResults;
+	for (const FOnlineSessionSearchResult El : LastSessionSearch->SearchResults)
+	{
+		FBlueprintSessionResult BlueprintSessionResult;
+		BlueprintSessionResult.OnlineResult = El;
+		BlueprintSessionResults.Add(BlueprintSessionResult);
+	}
+
+	OnFindSessionsCompleteEvent.Broadcast(BlueprintSessionResults, Successful);
+}
+
+void USessionSubsystem::JoinGameSession(const FBlueprintSessionResult& SessionResult)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnJoinGameSessionCompleteEvent.Broadcast(static_cast<EBPOnJoinSessionCompleteResult>(EOnJoinSessionCompleteResult::UnknownError));
+		return;
+	}
+
+	JoinSessionCompleteDelegateHandle =
+		SessionInterface->AddOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegate);
+
+	const ULocalPlayer* localPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+	if (!SessionInterface->JoinSession(*localPlayer->GetPreferredUniqueNetId(), NAME_GameSession, SessionResult.OnlineResult))
+	{
+		SessionInterface->ClearOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegateHandle);
+
+		OnJoinGameSessionCompleteEvent.Broadcast(static_cast<EBPOnJoinSessionCompleteResult>(EOnJoinSessionCompleteResult::UnknownError));
+	}
+}
+
+void USessionSubsystem::OnJoinSessionCompleted(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (SessionInterface)
+	{
+		SessionInterface->ClearOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegateHandle);
+	}
+
+	if (Result == EOnJoinSessionCompleteResult::Success)
+	{
+		TryTravelToCurrentSession();
+	}
+
+	OnJoinGameSessionCompleteEvent.Broadcast(static_cast<EBPOnJoinSessionCompleteResult>(Result));
+}
+
+bool USessionSubsystem::TryTravelToCurrentSession()
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		return false;
+	}
+
+	FString ConnectString;
+	if (!SessionInterface->GetResolvedConnectString(NAME_GameSession, ConnectString))
+	{
+		return false;
+	}
+
+	APlayerController* playerController = GetWorld()->GetFirstPlayerController();
+	playerController->ClientTravel(ConnectString, TRAVEL_Absolute);
+	return true;
+}
---
+++ Source/Kursova/ServerLogic/SessionSubsystem.h
@@ -0,0 +1,94 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "FindSessionsCallbackProxy.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "OnlineSessionSettings.h"
+#include "SessionSubsystem.generated.h"
+
+/**
+ * 
+ */
+
+UENUM(BlueprintType)
+enum class EBPOnJoinSessionCompleteResult : uint8
+{
+	Success,
+	SessionIsFull,
+	SessionDoesNotExist,
+	CouldNotRetrieveAddress,
+	AlreadyInSession,
+	UnknownError
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCSOnCreateSessionComplete, bool, Successful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCSOnUpdateSessionComplete, bool, Successful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCSOnStartSessionComplete, bool, Successful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCSOnEndSessionComplete, bool, Successful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCSOnDestroySessionComplete, bool, Successful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FCSOnFindSessionsComplete, const TArray<FBlueprintSessionResult>&, SessionsResult, bool, Successful);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCSOnJoinSessionComplete, EBPOnJoinSessionCompleteResult, Result);
+
+UCLASS()
+class USessionSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	USessionSubsystem();
+
+	UFUNCTION(BlueprintCallable) void CreateSession(int32 NumPublicConnections, bool IsLANMatch);
+	UFUNCTION(BlueprintCallable) void UpdateSession();
+	UFUNCTION(BlueprintCallable) void StartSession();
+	UFUNCTION(BlueprintCallable) void EndSession();
+	UFUNCTION(BlueprintCallable) void DestroySession();
+	UFUNCTION(BlueprintCallable) void FindSessions(int32 MaxSearchResults, bool IsLANQuery);
+	UFUNCTION(BlueprintCallable) void JoinGameSession(const FBlueprintSessionResult& SessionResult);
+	UFUNCTION(BlueprintCallable) bool SessionHasBeenStarted();
+	
+	UPROPERTY(BlueprintAssignable) FCSOnCreateSessionComplete OnCreateSessionCompleteEvent;
+	UPROPERTY(BlueprintAssignable) FCSOnUpdateSessionComplete OnUpdateSessionCompleteEvent;
+	UPROPERTY(BlueprintAssignable) FCSOnStartSessionComplete OnStartSessionCompleteEvent;
+	UPROPERTY(BlueprintAssignable) FCSOnEndSessionComplete OnEndSessionCompleteEvent;
+	UPROPERTY(BlueprintAssignable) FCSOnDestroySessionComplete OnDestroySessionCompleteEvent;
+	UPROPERTY(BlueprintAssignable) FCSOnFindSessionsComplete OnFindSessionsCompleteEvent;
+	UPROPERTY(BlueprintAssignable) FCSOnJoinSessionComplete OnJoinGameSessionCompleteEvent;
+
+protected:
+	void OnCreateSessionCompleted(FName SessionName, bool Successful);
+	void OnUpdateSessionCompleted(FName SessionName, bool Successful);
+	void OnStartSessionCompleted(FName SessionName, bool Successful);
+	void OnEndSessionCompleted(FName SessionName, bool Successful);
+	void OnDestroySessionCompleted(FName SessionName, bool Successful);
+	void OnFindSessionsCompleted(bool Successful);
+	void OnJoinSessionCompleted(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	bool TryTravelToCurrentSession();
+	
+
+private:
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+	TSharedPtr<FOnlineSessionSettings> LastSessionSettings;
+
+	FOnUpdateSessionCompleteDelegate UpdateSessionCompleteDelegate;
+  	FDelegateHandle UpdateSessionCompleteDelegateHandle;
+
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+	FDelegateHandle StartSessionCompleteDelegateHandle;
+
+	FOnEndSessionCompleteDelegate EndSessionCompleteDelegate;
+	FDelegateHandle EndSessionCompleteDelegateHandle;
+
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+	FDelegateHandle DestroySessionCompleteDelegateHandle;
+
+	FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+	FDelegateHandle FindSessionsCompleteDelegateHandle;
+	TSharedPtr<FOnlineSessionSearch> LastSessionSearch;
+
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FDelegateHandle JoinSessionCompleteDelegateHandle;
+};
