eb85259f5cf150b08dd50aceac17fb9e2e2407c0
Danylo Shlomiak <chotkiydanik@gmail.com>
2024-04-23 18:35:03 +0300

    Radio singletone fixed (#32)

--- Source/Kursova/Singletones/Radio.cpp
+++ Source/Kursova/Singletones/Radio.cpp
@@ -1,14 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Radio.h"
 
-ARadio* ARadio::InitializeRadio()
+ARadio* ARadio::SelfInstance = nullptr;
+
+ARadio::ARadio()
+{
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Capsule Collider"));
+	RootComponent = BoxComponent;
+	
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Skeletal Mesh"));
+	SkeletalMeshComponent->SetupAttachment(RootComponent);
+}
+
+ARadio* ARadio::GetInstance(UWorld* World, TSubclassOf<ARadio> RadioClass)
 {
 	if(SelfInstance == nullptr)
 	{
-		SelfInstance = CreateDefaultSubobject<ARadio>(TEXT("Radio"));
+		SelfInstance = World->SpawnActor<ARadio>(RadioClass, FVector::ZeroVector, FRotator::ZeroRotator);
 	}
-
+	
 	return SelfInstance;
 }
